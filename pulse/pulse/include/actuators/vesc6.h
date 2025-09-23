1469cf1d5ce52c4692c52e4a3c63fbb8edb5baac
nedarass <nedaras10@gmail.com>
2025-09-23 10:44:08 +0300

    Create vesc6.h

---
+++ pulse/pulse/include/actuators/vesc6.h
@@ -0,0 +1,12 @@
+#ifndef VESC6_H
+#define VESC6_H
+#include "pico/stdlib.h"
+#include "stdio.h"
+#include "../default_pins.h"
+#include "hardware/pwm.h"
+
+void vesc6_configure(); //connect pico <-> vesc 6
+uint8_t calculate_checksum(uint8_t *data, int len); //veri butunlugunu dogrulamak icin checksum hazirlilyor
+void set_motor_rpm(int rpm); //motor donus hizlarini ayarliyor
+
+#endif
