d2f3158bc4699b634f786cbf318df35fe0e7c85a
nedarass <nedaras10@gmail.com>
2025-09-23 11:13:13 +0300

    Create control.c

---
+++ pulse/pulse/src/control/control.c
@@ -0,0 +1,8 @@
+#include "../../include/control/control.h"
+#include "../../include/control/communication.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int get_message_type_non_sdk(int8_t * data, int size)
+{
+}
