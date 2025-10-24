fc8a4dbce25f740ecd02a8c1904203189765d0ab
dilatoptas <e230503017@stud.tau.edu.tr>
2025-10-24 16:12:28 +0300

    Implement timer start, stop, and PWM duty functions

--- pulse/pulse/hal/hal_timer.c
+++ pulse/pulse/hal/hal_timer.c
@@ -1,1 +1,17 @@
+#include "hal_timer.h"
+
+void hal_timer_start(TIM_HandleTypeDef *htim)
+{
+    HAL_TIM_Base_Start(htim);
+}
+
+void hal_timer_stop(TIM_HandleTypeDef *htim)
+{
+    HAL_TIM_Base_Stop(htim);
+}
+
+void hal_pwm_set_duty(TIM_HandleTypeDef *htim, uint32_t channel, uint16_t duty)
+{
+    __HAL_TIM_SET_COMPARE(htim, channel, duty);
+}
 
