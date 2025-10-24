ef85664929da3646236d01869cf49a1106e44b64
dilatoptas <e230503017@stud.tau.edu.tr>
2025-10-24 16:07:10 +0300

    Add I2C HAL header file with function declarations

--- pulse/pulse/hal/hal_i2c.h
+++ pulse/pulse/hal/hal_i2c.h
@@ -1,1 +1,19 @@
+#ifndef HAL_I2C_H
+#define HAL_I2C_H
+
+#include "stm32f4xx_hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hal_i2c_init(void);
+HAL_StatusTypeDef hal_i2c_write(uint8_t dev_addr, uint8_t *data, uint16_t size);
+HAL_StatusTypeDef hal_i2c_read(uint8_t dev_addr, uint8_t *data, uint16_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HAL_I2C_H
 
