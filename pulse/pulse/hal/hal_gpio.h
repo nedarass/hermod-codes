057513ccffa08e3b220b57fbe9ce88e00be526da
dilatoptas <e230503017@stud.tau.edu.tr>
2025-10-24 16:04:30 +0300

    Add GPIO HAL header file with initialization functions

--- pulse/pulse/hal/hal_gpio.h
+++ pulse/pulse/hal/hal_gpio.h
@@ -1,1 +1,20 @@
+#ifndef HAL_GPIO_H
+#define HAL_GPIO_H
+
+#include "stm32f4xx_hal.h"
+#include "board_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void hal_gpio_init(GPIO_TypeDef *port, uint16_t pin, uint32_t mode);
+void hal_gpio_write(GPIO_TypeDef *port, uint16_t pin, uint8_t value);
+uint8_t hal_gpio_read(GPIO_TypeDef *port, uint16_t pin);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HAL_GPIO_H
 
