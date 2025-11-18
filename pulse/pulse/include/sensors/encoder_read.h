d43a4a34c250b7511318d56c61e303d47347e7dc
dilatoptas <e230503017@stud.tau.edu.tr>
2025-11-18 15:24:58 +0300

    Add encoder driver header with initialization functions

--- pulse/pulse/include/sensors/encoder_read.h
+++ pulse/pulse/include/sensors/encoder_read.h
@@ -1,3 +1,4 @@
+/*
 #ifndef ENCODER_READ_H
 #define ENCODER_READ_H
 
@@ -24,4 +25,32 @@ int32_t  encoder_get_position(void);
 }
 #endif
 
-#endif /* ENCODER_READ_H */
+#endif
+*/
+// Core/Inc/encoder_driver.h
+
+#ifndef INC_ENCODER_DRIVER_H_
+#define INC_ENCODER_DRIVER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+
+// Encoder (TIM) Handle'ını parametre olarak alır
+void ENCODER_Init(TIM_HandleTypeDef *htim);
+
+// Pozisyonu okur ve shared_data'ya yazar
+void ENCODER_Update(TIM_HandleTypeDef *htim);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_ENCODER_DRIVER_H_ */
+
+
+/* HAL TIM Encoder Modu: PIO'nun yerine, STM32'nin donanımsal Timer Encoder
+Modu kullanılmalıdır. Bu, CPU yükü olmadan pozisyon sayımı
+için en verimli yöntemdir.*/
