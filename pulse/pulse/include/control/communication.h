175f5df18968d3c7fb9df36a5e4cad9321055d4c
dilatoptas <e230503017@stud.tau.edu.tr>
2025-11-18 15:23:54 +0300

    Refactor communication.h for improved structure

--- pulse/pulse/include/control/communication.h
+++ pulse/pulse/include/control/communication.h
@@ -1,4 +1,4 @@
-#ifndef COMMUNICATION_H
+/*#ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
 #include "pico/stdlib.h"
@@ -13,4 +13,36 @@ int read_protocol_data();
 
 void write_protocol_data(uint8_t* data, int size);
 
+#endif*/
+
+// Core/Inc/communication.h
+
+#ifndef INC_COMMUNICATION_H_
+#define INC_COMMUNICATION_H_
+
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+#include "main.h"
+#include "communication_ids.h"
+
+// Harici UART Handle'ı (main.c'de tanımlanır)
+extern UART_HandleTypeDef huart1;
+extern uint8_t rx_data;
+
+
+// Fonksiyon prototipleri
+void COMM_Init(void); // UART kesmeli alımı başlatır
+void COMM_ProcessByte(uint8_t ch); // UART RxCpltCallback'dan çağrılır
+
+// Telemetri Gönderimi
+void COMM_TransmitTelemetry(uint8_t id, uint8_t type, const void *payload, uint16_t payload_len);
+void COMM_SendHealthCheck(void);
+void COMM_SendTelemetryData(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_COMMUNICATION_H_ */
