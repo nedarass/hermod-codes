d67300036645f14aaf1a58c8c59e9f170aab0ef2
dilatoptas <e230503017@stud.tau.edu.tr>
2025-11-18 15:16:28 +0300

    Add TCPClient class definition in tcp_client.h

--- bifrost/desktop/include/tcp_client.h
+++ bifrost/desktop/include/tcp_client.h
@@ -52,3 +52,36 @@ private:
 };
 
 #endif // SERIAL_COMMUNICATOR_H
+
+/* #ifndef TCPCLIENT_H
+#define TCPCLIENT_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include "udp_listener.h"
+
+class TCPClient : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(bool isConnected READ isConnected NOTIFY connectionChanged)
+
+public:
+    explicit TCPClient(QObject *parent = nullptr);
+
+    Q_INVOKABLE void connectToServer(QString host, int port);
+    Q_INVOKABLE void sendMessage(const QString &message);
+    bool isConnected() const;
+
+signals:
+    void connectionChanged();
+    void messageReceived(const QString &message);
+
+private slots:
+    void onConnected();
+    void onDisconnected();
+    void onReadyRead();
+
+private:
+    QTcpSocket *socket;
+};
+
+#endif*/
