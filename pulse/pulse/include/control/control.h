af53b858f9c3ea83db88c579f0765dbe778cd741
dilatoptas <e230503017@stud.tau.edu.tr>
2025-11-18 15:24:13 +0300

    Update control.h

--- pulse/pulse/include/control/control.h
+++ pulse/pulse/include/control/control.h
@@ -1,4 +1,4 @@
-#ifndef CONTROL_H
+/*#ifndef CONTROL_H
 #define CONTROL_H
 
 #include "pico/stdlib.h"
@@ -15,4 +15,24 @@ enum MessageType
 
 int get_message_type_non_sdk(int8_t * data, int size);
 
+#endif*/
+
+// Core/Inc/control.h
+
+#ifndef INC_CONTROL_H_
+#define INC_CONTROL_H_
+
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+#include "main.h"
+
+void CONTROL_Init(void);
+void CONTROL_Loop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_CONTROL_H_ */
