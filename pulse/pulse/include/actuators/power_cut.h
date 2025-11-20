e2d541a4d40ce550c493e49da8a4efa18ee8e633
Neda Aras <nedaras10@gmail.com>
2025-11-20 22:39:33 +0300

    Remove unused GPIO definitions and clean comments

--- pulse/pulse/include/actuators/power_cut.h
+++ pulse/pulse/include/actuators/power_cut.h
@@ -10,10 +10,6 @@ extern "C" {
 #include "main.h"
 #include "shared_data.h"
 
-// Safety Relay GPIO
-#define SAFETY_RELAY_GPIO_PORT  GPIOC
-#define SAFETY_RELAY_PIN        GPIO_PIN_3
-
 typedef enum {
     POWER_STATE_OFF = 0,
     POWER_STATE_ON = 1,
@@ -23,14 +19,9 @@ typedef enum {
 void POWERCUT_Init(void);
 bool POWERCUT_SetState(bool enable_power);
 /* Ana güç rölesini kontrol eder.
-  enable_power:
-  true  -> Güç ver (Röle Çekili / İletimde)
-  false -> GÜCÜ KES (Röle Bırakılı / Kesik)
- */
+  enable_power: true  -> Güç ver (Röle Çekili / İletimde) false -> GÜCÜ KES (Röle Bırakılı / Kesik) */
 void POWERCUT_TriggerEmergency(void);
-/* Yazılımsal olarak gücü anında keser.
- Genellikle hata durumlarında (Watchdog vb.) çağrılır.
- */
+/* Yazılımsal olarak gücü anında keser. Genellikle hata durumlarında (Watchdog vb.) çağrılır.*/
 PowerState_t POWERCUT_GetState(void);
 
 #ifdef __cplusplus
