9f9baa304f1cf097f10f007e577e05a65d0fd4d5
dilatoptas <e230503017@stud.tau.edu.tr>
2025-11-22 15:31:14 +0300

    Refactor Omron sensor definitions and add new functions
    Removed obsolete Omron sensor definitions and state structure. Added new functions for emergency obstacle detection.

--- pulse/pulse/include/sensors/optics.h
+++ pulse/pulse/include/sensors/optics.h
@@ -9,37 +9,19 @@ extern "C" {
 #include "main.h"
 #include "shared_data.h"
 
-// Omron Sensör Pin Tanımları - CubeMX'te bu pinleri yapılandır!
-// bunları pin de yapabilrim ? 
-#define OMRON_SENSOR_1_PIN       GPIO_PIN_0
-#define OMRON_SENSOR_1_PORT      GPIOA
-#define OMRON_SENSOR_2_PIN       GPIO_PIN_1  
-#define OMRON_SENSOR_2_PORT      GPIOA
-
-// Omron Sensör Durumları
-typedef enum {
-    OMRON_OBSTACLE_DETECTED = 0,    // Engel var (LOW)
-    OMRON_NO_OBSTACLE = 1           // Engel yok (HIGH)
-} Omron_State_t;
-
-// Omron Sensör Yapısı
-typedef struct {
-    GPIO_TypeDef* GPIO_Port;
-    uint16_t GPIO_Pin;
-    Omron_State_t last_state;
-    uint32_t detection_timestamp;
-    uint8_t sensor_id;
-} Omron_Sensor_t;
+// ... diğer tanımlamalar aynı
 
 // Fonksiyon Prototipleri
 void OPTICS_Init(void);
 void OPTICS_Update(void);
+void OPTICS_EXTI_Callback(uint16_t GPIO_Pin);
+
+// ✅ YENİ EKLENDİ - Control.c bunları kullanacak
+uint8_t OPTICS_IsEmergencyObstacleDetected(void);
+void OPTICS_ClearEmergencyFlag(void);
 Omron_State_t OPTICS_GetSensorState(uint8_t sensor_id);
 uint32_t OPTICS_GetDetectionTime(uint8_t sensor_id);
 uint8_t OPTICS_IsAnyObstacleDetected(void);
-
-// EXTI Callback - main.c'deki HAL_GPIO_EXTI_Callback'tan çağrılacak
-void OPTICS_EXTI_Callback(uint16_t GPIO_Pin);
 
 #ifdef __cplusplus
 }
