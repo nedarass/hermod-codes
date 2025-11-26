1eed7a6df873f9645abc7b8657cfd97c6617e1cf
dilatoptas <e230503017@stud.tau.edu.tr>
2025-11-26 18:11:57 +0300

    Update shared_data.c

--- pulse/pulse/src/shared_data.c
+++ pulse/pulse/src/shared_data.c
@@ -31,9 +31,10 @@ void __attribute__((constructor)) init_system_state(void) {
     g_system_state.brake_status = 0;      // Frenler açık (serbest) başlasın
 }
 
-/* /* Core/Src/shared_data.c */
+/* 
 #include "shared_data.h"
 
 // Tüm sistemin kullanacağı devasa veri yapısı burada RAM'de oluşturulur.
 // Başlangıçta her şey 0 olsun diye {0} ile başlatıyoruz.
-shared_data_t shared_data = {0};*/
+shared_data_t shared_data = {0};
+*/
