51a1b665edac0debac410dabe8a90433d7f919e9
Neda Aras <nedaras10@gmail.com>
2025-11-27 20:26:41 +0300

    Update inverter_rs485.h

--- pulse/pulse/include/actuators/inverter_rs485.h
+++ pulse/pulse/include/actuators/inverter_rs485.h
@@ -37,24 +37,3 @@ InverterState_t INVERTER_GetState(void);
 #endif
 
 #endif
-
-
-
-
-
-
-
-/*#ifndef INVERTER_RS485_H
-#define INVERTER_RS485_H
-
-#include "pico/stdlib.h"
-#include "stdio.h"
-#include "../default_pins.h"
-
-void rs485_modbus_configure(); //modbus iletisimi icin konfigurasyon
-
-// rs485 modbus üzerinden veri gönderme fonksiyonu
-void rs485_write(uint8_t *message, uint8_t len);
-
-#endif
-*/
