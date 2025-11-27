631a4118ab006fb71d887ea096dbe307e42cf1ed
Neda Aras <nedaras10@gmail.com>
2025-11-27 19:25:16 +0300

    Clean up blinker.h by removing unused code
    Removed commented-out code and unnecessary includes from blinker.h

--- pulse/pulse/include/actuators/blinker.h
+++ pulse/pulse/include/actuators/blinker.h
@@ -35,22 +35,3 @@ void BLINKER_StopBlinking(void) ;
 
 #endif
 
-
-
-
-
-
-
-/*#ifndef BLINKER_H
-#define BLINKER_H
-
-#include "pico/stdlib.h"
-#include "stdio.h"
-#include "../default_pins.h"
-
-void blinker_configure_pin(); //fren erisimi icin gpio pinlerini hazirlar
-
-void set_blinker(bool status); //fren ciktilarini ayarlar. ornegin gpio x  1 versin vs.
-
-#endif
-*/
