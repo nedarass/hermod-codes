d79be19c4d17dab412ac9b1f0e928cf91ed70b16
Neda Aras <nedaras10@gmail.com>
2025-12-01 09:37:35 +0300

    Update control.h

--- pulse/pulse/include/control/control.h
+++ pulse/pulse/include/control/control.h
@@ -1,23 +1,3 @@
-/*#ifndef CONTROL_H
-#define CONTROL_H
-
-#include "pico/stdlib.h"
-#include "hardware/timer.h"
-#include <stdio.h>
-#include "pico/stdlib.h"
-
-enum MessageType 
-{
-    PING_PONG = 0,
-    REQUEST = 1,
-    WRITE = 2
-};
-
-int get_message_type_non_sdk(int8_t * data, int size);
-
-#endif*/
-
-// Core/Inc/control.h
 
 #ifndef INC_CONTROL_H_
 #define INC_CONTROL_H_
