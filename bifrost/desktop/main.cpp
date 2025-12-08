6b034e39c83586453e27381f7f6b230d18f3e455
Neda Aras <nedaras10@gmail.com>
2025-12-08 19:34:00 +0300

    Update main.cpp

--- bifrost/desktop/main.cpp
+++ bifrost/desktop/main.cpp
@@ -1,10 +1,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QDir>
 #include "include/udp_listener.h"
 #include "include/tcp_client.h"
-//
-#include "include/serial_communicator.h"
+
 
 int main(int argc, char *argv[])
 {
@@ -17,28 +17,26 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     // context property leri modu eklemeeden once cagir
+
     engine.rootContext()->setContextProperty("UDPListener", &udp_listener);
     engine.rootContext()->setContextProperty("TCPClient", &client);
+
+
+
+    QString mainQmlPath = "/Users/ubeydhamzatoptas/Downloads/hermod-codes-stm32/bifrost/desktop/Main.qml";
+    qDebug() << "Loading QML from ABSOLUTE path:" << mainQmlPath;
+    qDebug() << "File exists:" << QFile::exists(mainQmlPath);
+    engine.load(QUrl::fromLocalFile(mainQmlPath));
 
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
-        []() { QCoreApplication::exit(-1); },
+        []() {
+            qDebug() << "QML loading failed!";
+            QCoreApplication::exit(-1);
+        },
         Qt::QueuedConnection);
-
-    // serial communication
-
-    SerialCommunicator serial;
-    serial.openPort("/dev/ttyUSB0", 115200); // test için port adı değiştir
-
-
-    engine.rootContext()->setContextProperty("SerialCommunicator", &serial);
-
-
-    engine.loadFromModule("desktop", "Main");
-
-    
 
     return app.exec();
 }
