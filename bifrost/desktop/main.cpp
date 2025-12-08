c36d9ce00753f83c7a1595cb8c64d4cc2a92876a
Neda Aras <nedaras10@gmail.com>
2025-12-08 11:04:33 +0300

    Update main.cpp

--- bifrost/desktop/main.cpp
+++ bifrost/desktop/main.cpp
@@ -3,6 +3,8 @@
 #include <QQmlContext>
 #include "include/udp_listener.h"
 #include "include/tcp_client.h"
+//
+#include "include/serial_communicator.h"
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +26,15 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
+    // serial communication
+
+    SerialCommunicator serial;
+    serial.openPort("/dev/ttyUSB0", 115200); // test için port adı değiştir
+
+
+    engine.rootContext()->setContextProperty("SerialCommunicator", &serial);
+
 
     engine.loadFromModule("desktop", "Main");
 
